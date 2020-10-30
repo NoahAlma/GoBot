94ba20f74a34625b3081172a33d1f91aa567c0ce
Noah <40533028+noahalma@users.noreply.github.com>
2020-10-30 19:32:12 -0400

    Create handlers.go

---
+++ client/handlers.go
@@ -0,0 +1,46 @@
+package wh
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"log"
+	"strings"
+)
+
+func messageCreateHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// In DM, prefix is not needed
+	if m.GuildID != "" && !strings.HasPrefix(m.Content, "wh?") {
+		return
+	}
+
+	content := strings.TrimLeft(strings.TrimSpace(m.Content), "wh?")
+	for _, c := range wh.commands {
+
+		for _, alias := range c.Aliases() {
+			if strings.HasPrefix(content, alias) {
+				err := c.Run(s, m.Message)
+				if err != nil {
+					log.Println(err)
+				}
+				return
+			}
+		}
+	}
+
+	return
+
+}
+
+
+func onReady(s *discordgo.Session, ready *discordgo.Ready) {
+	fmt.Println("WH-Bot is running.")
+	fmt.Println(ready.User.String())
+	fmt.Println("ID: " + ready.User.ID)
+	fmt.Println("\t" + strings.Repeat("-", 30) + "\t")
+	return
+}
