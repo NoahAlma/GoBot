21a28131ecc86e0113bab9eb42f251a2ae45146b
Noah <40533028+noahalma@users.noreply.github.com>
2020-11-11 10:25:01 -0500

    Update handlers.go

--- client/handlers.go
+++ client/handlers.go
@@ -12,8 +12,7 @@ func messageCreateHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-
-	// In DM, prefix is not needed
+	
 	if m.GuildID != "" && !strings.HasPrefix(m.Content, "wh?") {
 		return
 	}
